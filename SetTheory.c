e783920fc6e5530fe03e50cbd5d68d5ca6b00439
8ahmadreza3 <8arhp3@gmail.com>
2024-03-18 13:08:23 +0330

    fix algorithm

--- SetTheory.c
+++ SetTheory.c
@@ -190,3 +190,120 @@ char Motammem(char u[200] ,char member[10][50], int s2)
   print(res);
   return ;
 }
+
+char Tafozol(char member[10][50], int s1 , int s2)
+{
+  char res[200];
+  int flag=0;
+  for(int r=0 , x=0 ; member[s1][r]!=NULL ; r++){
+    for(int b=0 ; member[s2][b]!=NULL ; b++){
+      if(member[s1][r]==member[s2][b]){
+        ++flag;
+      }
+    }
+    if(flag==0){
+      res[x]=member[s1][r];
+      ++x;
+    }
+  }
+  print(res);
+  return;
+}
+
+char find3(char name[10][5] , char a[50])
+{
+  int s=-1 , x;
+  for(int y=0 ; y<10 ; y++){
+    x=0;
+    for(int g=0 ; name[y][g]!='\0' ;g++){
+      if(name[y][g]==a[g]){
+        x++;
+      }
+      if(x==strlen(a)){
+        s=y;
+        break;
+      }
+    }
+  }
+  if(s==-1){
+    printf("Not defined");
+  }
+  return s;
+}
+
+int main()
+{
+  char name[10][5] , member[10][50] , u[200] , umember[10][50];
+  char a[50];
+  int set1 , set2 , line=0 ;
+    for(int i=0 ; i>-1 ; ++i){
+      scanf("%s", a);
+      int flag=0 ;
+      if(!strcmp(a , "EOF")){
+        printf("End program");
+        break;
+      }
+      if(a=='U'){
+        strcpy(member , umember);
+        Universal(umember , u);
+        print(u);
+      }
+      else{
+        for(int j=0 ; a[j]!='\0' ; j++){
+          if(a[j]=='='){
+            Joda(a , line , member , name);
+            remove_set(member , line);
+            ++line;
+            ++flag;
+          }
+          else{
+            if(a[j]=='+'){
+              set1=find1(name ,a , "+");
+              set2=find2(name ,a , "+");
+              if(set1==-1||set2==-1){
+                continue;
+              }
+              ejtema(member , set1 , set2);
+              ++flag;
+            }
+            if(a[j]=='*'){
+              set1=find1(name ,a , "*");
+              set2=find2(name ,a , "*");
+              if(set1==-1||set2==-1){
+                continue;
+              }
+              eshterak(member , set1 , set2);
+              ++flag;
+            }
+            if(a[0]=='-'){
+              strcpy(member , umember);
+                Universal(umember , u);
+                set2=find2(name ,a , "-");
+                if(set2==-1){
+                  continue;
+                }
+                Motammem(u , member , set2 );
+                ++flag;
+                }
+                else if(a[j]=='-'){
+                  set1=find1(name ,a , "-");
+                  set2=find2(name ,a , "-");
+                  if(set1==-1 || set2==-1){
+                    continue;
+                }
+                Tafozol(member , set1 , set2);
+                ++flag;
+                }
+          }
+        }
+        if(flag==0){
+          set1=find3(name,a);
+          if(set1==-1){
+            continue;
+          }
+          printset(member , set1);
+        }
+      }
+  }
+  return 0;
+}
